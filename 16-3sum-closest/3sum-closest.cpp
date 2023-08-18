f6f91ea92e64926abc6bb21a91f4624407330670
I'm ÀŚHÌŚH  MÀŁEY <90534593+Ashishmaley@users.noreply.github.com>
2023-08-18 16:19:08 +0530

    Time: 74 ms (15.58%) | Memory: 10.2 MB (57.77%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+       int ans=nums[0]+nums[1]+nums[2];
+       if(ans==target)return target;
+       for(int i=0;i<nums.size()-2;i++){
+           int j=i+1;
+           int k=nums.size()-1;
+           while(j<k){
+               int l=nums[i]+nums[j]+nums[k];
+               if(abs(target-l)<abs(target-ans)){
+                   ans=l;
+               }
+               if(nums[i]+nums[j]+nums[k]>target){
+                   k--;
+               }else if(nums[i]+nums[j]+nums[k]<target){
+                   j++;
+               }else return target;
+           }
+       }
+       return ans; 
+    }
+};

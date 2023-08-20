6305ea9035fb4b27aec188b8349b6f3e511fb5ee
I'm ÀŚHÌŚH  MÀŁEY <90534593+Ashishmaley@users.noreply.github.com>
2023-08-20 15:04:42 +0530

    Time: 15 ms (36.21%) | Memory: 18.5 MB (27.27%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0,j=1;
+        while(j<nums.size()){
+            if(nums[i]!=nums[j]){
+                i++;
+                swap(nums[i],nums[j]);
+            }
+            j++;
+        }
+        return i+1;
+    }
+};

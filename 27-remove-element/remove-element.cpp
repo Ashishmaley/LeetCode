2d0aed253eaf652c87e0f9796c888747690bac1b
I'm ÀŚHÌŚH  MÀŁEY <90534593+Ashishmaley@users.noreply.github.com>
2023-08-20 19:00:07 +0530

    Time: 4 ms (25.23%) | Memory: 8.8 MB (48.59%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int n=nums.size();
-        if(n<1)return 0;
-        int i=0,j=n-1;
-        while(i<=j){
-            if(nums[i]==val){
-                while(j>=0&&nums[j]==val){
-                    j--;
-                    }
-                if(i<j&&nums[j]!=val){
-                    nums[i]=nums[j];
-                    j--;
-                }
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                nums[j]=nums[i];
+                j++;
             }
-            else{    
-                i++;
-                }
         }
-        return i;   
+        return j;   
     }
 };

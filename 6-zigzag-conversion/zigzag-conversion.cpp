6eb32429cd133f15ca70e47051faf48c72dbe36f
I'm ÀŚHÌŚH  MÀŁEY <90534593+Ashishmaley@users.noreply.github.com>
2024-05-28 20:31:08 +0530

    Time: 34 ms (13.33%) | Memory: 19.4 MB (11.82%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     string convert(string s, int n) {
-        if(n==1||n>s.length())
-        return s;
-        string ans="";
-        bool flag=false;
-        vector<string>v(n);
-        int i=0;
-        for(auto ele:s){
-            if(i==0)
-            {
-                flag=true;
-                v[i]+=ele;
-                i++;
-                continue;
-            }
-            if(i==n-1){
-                flag=false;
-                v[i]+=ele;
-                i--;
-                continue;
-            }
-            if(flag==true){
-                v[i]+=ele;
-                i++;
+        if(n==1)return s;
+        vector<vector<char>>v(n);
+        int play=0;
+        bool down=true;
+        for(int i=0;i<s.size();i++){
+            if(down){
+                v[play].push_back(s[i]);
+                if(play==n-1&&down==true){
+                    down = false;
+                    play--;
+                }
+                else
+                play++;
             }
             else{
-                v[i]+=ele;
-                i--;
+                v[play].push_back(s[i]);
+                if(play==0&&down!=true){
+                    down = true;
+                    play++;
+                }else 
+                play--;
             }
         }
-        for(auto l:v){
-            ans+=l;
-        }
+        string ans = "";
+            for(auto ele:v){
+                string str = "";
+                for (auto ch : ele) {
+                    str += ch; 
+                }
+                ans += str;
+            }
         return ans;
-    }     
+    }
 };
